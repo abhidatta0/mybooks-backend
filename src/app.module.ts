f75b8af423c4dde07b7ba0983ce843fe971ec5fe
abhidatta0 <abhidatta146@gmail.com>
2024-04-20 13:12:36 +0530

    getting access token in cookie

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, BooksModule]
+  imports: [ConfigModule.forRoot({isGlobal: true}),JwtModule.register({global: true}) ,
+    DatabaseModule, UsersModule, BooksModule]
 })
 export class AppModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService, private readonly jwtService: JwtService, private readonly configService: ConfigService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    const data =  this.usersService.create(createUserDto);
-    return data;
+  async create(@Body() createUserDto: CreateUserDto) {
+    await this.usersService.create(createUserDto);
+    return {success: true};
   }
 
   @Get()
@@ -31,5 +33,10 @@ export class UsersController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
+  }
+
+  private getAccessToken(userId: number){
+    const payload = {sub: userId};
+    return this.jwtService.signAsync(payload,{secret: this.configService.get("JWT_ACCESS_TOKEN_SECRET")});
   }
 }
