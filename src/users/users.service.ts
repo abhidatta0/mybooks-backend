58d8578884d1d418234435df862b66f64a117660
abhidatta0 <abhidatta146@gmail.com>
2024-04-23 09:49:37 +0530

    updating a user api

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,6 +6,7 @@ import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { LoginUserDto } from './dto/login-user.dto';
+import { CustomBadRequestException } from 'src/common/exceptions/CustomBadRequestException';
 
 @Injectable()
 export class UsersService {
@@ -36,7 +37,7 @@ export class UsersService {
   }
 
   findOne(email: string) {
-    return this.userRepository.findOneOrFail({
+    return this.userRepository.findOne({
       where:{
         email
       },
@@ -44,8 +45,33 @@ export class UsersService {
     });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  findOneById(id: number) {
+    return this.userRepository.findOne({
+      where:{
+        id
+      },
+    });
+  }
+
+  findOneByUserName(username:string) {
+    return this.userRepository.findOne({
+      where:{
+        username,
+      },
+    });
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.findOneById(id);
+    if(!user){
+      throw new CustomBadRequestException("This user doesnot exist!");
+    }
+    const matchedUsername = await this.findOneByUserName(updateUserDto.username);
+    if(matchedUsername){
+      throw new CustomBadRequestException("Username is already taken.");
+    }
+    await this.userRepository.update({id} , updateUserDto);
+    return this.findOneById(id);
   }
 
   remove(id: number) {
