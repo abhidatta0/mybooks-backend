78b94a8fba7b62537541c54b10bcdccb9a64b022
abhidatta0 <abhidatta146@gmail.com>
2024-04-24 10:07:51 +0530

    logging interceptor to measure total time of request-response

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,19 @@ import { BooksModule } from './books/books.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthGuard } from './auth/auth.guard';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { LoggerInterceptor } from './common/interceptors/logging.interceptor';
 
 @Module({
   imports: [ConfigModule.forRoot({isGlobal: true}),JwtModule.register({global: true}) ,
     DatabaseModule, UsersModule, BooksModule],
   providers:[
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor
+    },
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
---
+++ src/common/interceptors/logging.interceptor.ts
@@ -0,0 +1,11 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import {tap} from 'rxjs/operators';
+
+@Injectable()
+export class LoggerInterceptor implements NestInterceptor{
+    intercept(context: ExecutionContext, next: CallHandler){
+        console.log("Before...");
+        const now = Date.now()
+        return next.handle().pipe(tap(()=> console.log(`After...${Date.now()-now}ms`)))
+    }
+}
