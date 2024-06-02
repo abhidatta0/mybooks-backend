1b29dcc7a83f76d3345c808d0b603d5c0e5bc936
abhidatta0 <abhidatta146@gmail.com>
2024-06-02 11:19:10 +0530

    updated dto

--- src/books/dto/update-book.dto.ts
+++ src/books/dto/update-book.dto.ts
@@ -1,6 +1,8 @@
 import { Min } from 'class-validator';
+import { CreateBookDto } from './create-book.dto';
+import { PartialType,OmitType } from '@nestjs/mapped-types';
 
-export class UpdateBookDto  {
+export class UpdateBookDto  extends PartialType(OmitType(CreateBookDto,['user_id','total_number_of_pages'])){
     @Min(1)
     number_of_pages_read: number;
 }
