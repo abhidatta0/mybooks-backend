4060a455b51f9a5d9b34036c6e12bdb89e874f80
abhidatta0 <abhidatta146@gmail.com>
2024-06-03 00:21:54 +0530

    update logic

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -25,7 +25,7 @@ export class BooksService{
     if(!book){
       throw new CustomBadRequestException("This book doesnot exist!");
     }
-    if(book.total_number_of_pages <= updateBookDto.number_of_pages_read){
+    if(book.total_number_of_pages < updateBookDto.number_of_pages_read){
       throw new CustomBadRequestException("This action is invalid");
     }
     await this.bookRepository.updateOne({id} ,updateBookDto );
